fa01b1799c4ac9870978a84115145114e27abe92
Yulong Tan <swifties@uw.edu>
2015-10-18 08:24:22 -0700

    made an onclick method for the upload button and combined the activity result methods

--- app/src/main/java/yulongproductions/com/matchme/GameActivity.java
+++ app/src/main/java/yulongproductions/com/matchme/GameActivity.java
@@ -2,6 +2,8 @@ package yulongproductions.com.matchme;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,16 +13,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.IOException;
+
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+import com.clarifai.api.Tag;
+import com.clarifai.api.exception.ClarifaiException;
+
 
 public class GameActivity extends ActionBarActivity {
     public static final String TAG = GameActivity.class.getSimpleName();
     private static final int CAM_REQUEST = 1313;
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     private GrabAdjective mGrabAdjective = new GrabAdjective();
     private Dictionary mDictionary = new Dictionary();
@@ -32,7 +42,10 @@ public class GameActivity extends ActionBarActivity {
     private Button takePhoto;
     private Button logout;
     private Button defineWord;
+    private Button upload;
     private ImageView image;
+
+    private Bitmap bitmap;
 
 
     @Override
@@ -48,6 +61,7 @@ public class GameActivity extends ActionBarActivity {
         takePhoto = (Button)findViewById(R.id.cameraButton);
         logout = (Button)findViewById(R.id.logoutButton);
         defineWord = (Button)findViewById(R.id.defineButton);
+        upload = (Button)findViewById(R.id.uploadButton);
         image = (ImageView)findViewById(R.id.cameraImage);
 
         mTextView = (TextView)findViewById(R.id.adjTextView);
@@ -78,6 +92,16 @@ public class GameActivity extends ActionBarActivity {
                 Toast.makeText(GameActivity.this, adjective + ": ", Toast.LENGTH_LONG).show();
             }
         });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "Select Image"), PICK_IMAGE_REQUEST);
+            }
+        });
     }
 
     @Override
@@ -87,7 +111,14 @@ public class GameActivity extends ActionBarActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAM_REQUEST) {
+        Bundle bundle = data.getExtras();
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK &&
+                data != null && data.getData() != null) {
+            // The image
+            Uri uri = data.getData();
+
+            bitmap = loadBitmapFromUri(data.getData());
+        } else if (requestCode == CAM_REQUEST) {
             Bitmap thumbnail = (Bitmap)data.getExtras().get("data");
             image.setImageBitmap(thumbnail);
         }
@@ -123,5 +154,21 @@ public class GameActivity extends ActionBarActivity {
                 });
             }
         });
+    }
+
+
+    // From Demo
+    private Bitmap loadBitmapFromUri(Uri uri) {
+        try {
+            // The image may be large. Load an image that is sized for display. This follows best
+            // practices from http://developer.android.com/training/displaying-bitmaps/load-bitmap.html
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            //opts.inJustDecodeBounds = true;
+            //opts = new BitmapFactory.Options();
+            return BitmapFactory.decodeStream(getContentResolver().openInputStream(uri), null, opts);
+        } catch (IOException e) {
+            // Log.e(TAG, "Error loading image: " + uri, e);
+        }
+        return null;
     }
 }
