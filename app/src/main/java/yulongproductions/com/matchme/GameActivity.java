431ce7c68586faf03658622cef1f820a7fff8569
Yulong Tan <swifties@uw.edu>
2015-10-17 22:00:34 -0700

    created the main UI and created another activity

---
+++ app/src/main/java/yulongproductions/com/matchme/GameActivity.java
@@ -0,0 +1,16 @@
+package yulongproductions.com.matchme;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class GameActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/yulongproductions/com/matchme/MainActivity.java
+++ app/src/main/java/yulongproductions/com/matchme/MainActivity.java
@@ -1,38 +1,42 @@
 package yulongproductions.com.matchme;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Button mStartButton;
+    private EditText mNameText;
+    private EditText mFavText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button)findViewById(R.id.startButton);
+        mNameText = (EditText)findViewById(R.id.nameEditText);
+        mFavText = (EditText)findViewById(R.id.favEditText);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameText.getText().toString();
+                String fav = mFavText.getText().toString();
+                startGame(name, fav);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void startGame(String name, String fav) {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra(getString(R.string.name), name);
+        intent.putExtra(getString(R.string.fav), fav);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
