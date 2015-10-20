134624fb52db379d3eef0e67fb5c77002fd9425d
Yulong Tan <swifties@uw.edu>
2015-10-19 21:30:38 -0700

    removed unused rotate method

--- app/src/main/java/yulongproductions/com/matchme/GameActivity.java
+++ app/src/main/java/yulongproductions/com/matchme/GameActivity.java
@@ -220,12 +220,4 @@ public class GameActivity extends ActionBarActivity {
         int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
         return dp;
     }
-
-    public Bitmap RotateBitmap(Bitmap source, float angle)
-    {
-
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
-    }
 }
